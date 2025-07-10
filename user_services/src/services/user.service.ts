da481973a4ab6e07672c9a7144915dc0e3a4e947
abhishekchaurasiya <abhichek2580@gmail.com>
2025-07-10 12:40:16 +0530

    refactor: change message

--- user_services/src/services/user.service.ts
+++ user_services/src/services/user.service.ts
@@ -2,6 +2,7 @@ import { adminUrl } from "../config/config";
 import { OrderModel } from "../models/order.model";
 import { UserDoc, UserModel } from "../models/user.model";
 import { BadRequestError, InternalServerError } from "../utils/CustomError";
+import { resMsg } from "../utils/resMsg";
 import { placeOrderSchema, userRegisterSchema } from "../zod/user.schema";
 import axios from "axios";
 
@@ -10,15 +11,15 @@ export const UserServices = {
   async RegisterUser(payload: UserDoc) {
     const parsedPayload = userRegisterSchema.safeParse(payload);
     if (!parsedPayload.success) {
-      throw new BadRequestError("Invalid registration details");
+      throw new BadRequestError(resMsg.INVALID_RESIGTRATION_DETAILS);
     }
     const { name, email, phone } = parsedPayload.data;
 
     const existsUser = await UserModel.findOne({ email });
-    if (existsUser) throw new BadRequestError("User already exists");
+    if (existsUser) throw new BadRequestError(resMsg.USER_ALREADY_EXISTS);
 
     const user = new UserModel({ name, email, phone });
-    if (!user) throw new BadRequestError("User registration failed");
+    if (!user) throw new BadRequestError(resMsg.USER_REGISTRATION_FAILED);
     await user.save();
     return user;
   },
@@ -31,22 +32,22 @@ export const UserServices = {
   }) {
     const parsedPayload = placeOrderSchema.safeParse(payload);
     if (!parsedPayload.success) {
-      throw new BadRequestError("Invalid order details");
+      throw new BadRequestError(resMsg.INVALID_ORDER_DETAILS);
     }
     const { userId, productId, quantity } = parsedPayload.data;
 
     try {
       // Validate user
       const user = await UserModel.findById(userId).select({ _id: 1 });
-      if (!user) throw new BadRequestError("User not found");
+      if (!user) throw new BadRequestError(resMsg.USER_NOT_FOUND);
 
       // Validate product by Admin services
       const { data } = await axios.get(`${adminUrl}/product/${productId}`);
       const product = data.product;
-      if (!product) throw new BadRequestError("Product not found");
+      if (!product) throw new BadRequestError(resMsg.PRODUCT_NOT_FOUND);
 
       if (product.stock < quantity) {
-        throw new BadRequestError("Insufficient stock");
+        throw new BadRequestError(resMsg.INSUFFICIENT_STOCK);
       }
 
       // save the order to the database
@@ -56,7 +57,7 @@ export const UserServices = {
         quantity,
         createdAt: new Date(),
       });
-      if (!order) throw new BadRequestError("Order creation is failed");
+      if (!order) throw new BadRequestError(resMsg.ORDER_CREATION_FAILED);
       await order.save();
 
       // Notify the admin service to update the
@@ -67,7 +68,7 @@ export const UserServices = {
       return order;
     } catch (err: any) {
       if (err instanceof BadRequestError) throw err;
-      throw new InternalServerError("Something went wrong");
+      throw new InternalServerError(resMsg.SOMETHING_WENT_WRONG);
     }
   },
 };
