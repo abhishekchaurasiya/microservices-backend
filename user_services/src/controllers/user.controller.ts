b62a821549bdf371378654053e61c3b6cb608fdf
abhishekchaurasiya <abhichek2580@gmail.com>
2025-07-10 12:39:56 +0530

    refactor: add variable for error message

--- user_services/src/controllers/user.controller.ts
+++ user_services/src/controllers/user.controller.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from "express";
 import asyncHandler from "../helpers/asyncHandler";
-import { sanitizeInput } from "../helpers/sanitizeInput";
 import { UserServices } from "../services/user.service";
+import { resMsg } from "../utils/resMsg";
+import { CREATED } from "../utils/StatusCode";
 
 export const registerUser = asyncHandler(
   async (req: Request, res: Response) => {
-    const cleandata = sanitizeInput(req.body);
-    const user = await UserServices.RegisterUser(cleandata);
-    res.status(201).json(user);
+    const user = await UserServices.RegisterUser(req.body);
+    res.status(CREATED).json(user);
   }
 );
 
 export const placeOrder = asyncHandler(async (req: Request, res: Response) => {
   const order = await UserServices.PlaceOrder(req.body);
-  res.status(201).json({ message: "Successfully order placed.", order });
+  res
+    .status(CREATED)
+    .json({ message: resMsg.ORDER_PLACED_SUCCESSFULLY, order });
 });
